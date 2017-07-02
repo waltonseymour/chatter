0db97c9b252681746792d39bfb753d07b55406aa
Walton Seymour <waltonseymour@users.noreply.github.com>
2017-07-02 14:53:35 -0400

    Adds input group (#5)
    * adds form group
    
    * moves to ssl / wss
    
    * fixes dev

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as uuidv4 from 'uuid/v4';
 import { ChatRoom, ChatMessage } from './components/ChatRoom';
 import { NavHeader } from './components/NavHeader';
 import { List } from 'immutable';
@@ -9,7 +10,7 @@ require("./app.less");
 const App = () => {
     return <div>
         <NavHeader />
-        <ChatRoom wsUrl={"ws://" + location.host + "/chat"} />
+        <ChatRoom wsUrl={"wss://" + location.host + "/chat?user=" + uuidv4()} />
     </div>;
 }
 
